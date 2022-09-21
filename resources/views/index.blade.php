8b1ba3f1330aa37b9eb535d9af758b3d091e24af
surya <surya.065118107@unpak.ac.id>
2022-09-21 09:06:29 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.header')
+@section('content')
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #6f42c1;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="/logo/logo-unpak_menu_web.webp" alt="Bootstrap" width="50"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll bav-pills" style="--bs-scroll-height: 100px;">
+          <li>
+            <a href="/" class="nav-link active">Korps Mahawarman Universitas Pakuan Bogor</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Struktur Organisasi</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Kegiatan
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Pendidikan lanjutan</a></li>
+              <li><a class="dropdown-item" href="#">Pendidikan Berjenjang</a></li>
+              <li><a class="dropdown-item" href="#">Kursus</a></li>
+              <li><a class="dropdown-item" href="#">Refreshing</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Carousel -->
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="/image/menembak.jpeg" class="w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="/image/terjun1.jpeg" class="w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="/image/terjun2.jpeg" class="w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Korps Mahawarman Universitas Pakuan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+        @yield('content')
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
