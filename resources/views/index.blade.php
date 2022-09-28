9d02e789a8f39f52a90fde0f23d68f4fe05d129d
surya9393 <surya.065118107@unpak.ac.id>
2022-09-28 10:13:15 +0700

    revisi desain

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,67 +1,40 @@
-@extends('layouts.home')
+@extends('layouts.main')
 @section('content')
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #6f42c1;">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="/logo/logo-unpak_menu_web.webp" alt="Bootstrap" width="50"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarScroll">
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll bav-pills" style="--bs-scroll-height: 100px;">
-          <li>
-            <a href="/" class="nav-link active">Korps Mahawarman Universitas Pakuan Bogor</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Struktur Organisasi</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Kegiatan
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Pendidikan lanjutan</a></li>
-              <li><a class="dropdown-item" href="#">Pendidikan Berjenjang</a></li>
-              <li><a class="dropdown-item" href="#">Kursus</a></li>
-              <li><a class="dropdown-item" href="#">Refreshing</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+    
+<!-- ======= Header ======= -->
+@include('partials.header')
+  
+  <!-- ======= Banner Section ======= -->
+  @include('partials.banner')
+  <!-- End Hero -->
 
-  <!-- Carousel -->
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="/image/menembak.jpeg" class="w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="/image/terjun1.jpeg" class="w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="/image/terjun2.jpeg" class="w-100" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-@endsection
+  <main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    @include('partials.clients')
+    <!-- ======= About Us Section ======= -->
+    @include('partials.about')
+    
+    <!-- ======= Why Us Section ======= -->
+    @include('partials.whyus')
+    <!-- ======= Skills Section ======= -->
+    @include('partials.skills')
+    <!-- ======= Services Section ======= -->
+    @include('partials.services')
+    <!-- ======= Cta Section ======= -->
+    @include('partials.cta')
+    <!-- ======= Portfolio Section ======= -->
+    @include('partials.portofolio')
+    <!-- ======= Team Section ======= -->
+    @include('partials.team')
+    <!-- ======= Pricing Section ======= -->
+    @include('partials.pricing')
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    @include('partials.faq')
+    <!-- ======= Contact Section ======= -->
+    @include('partials.contact')
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  @include('partials.footer')
+  @endsection
